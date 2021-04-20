697cea319a99dec862fad248e079c761f6c73eac
Lassi Puurunen <36304967+plassi@users.noreply.github.com>
2021-04-21 02:00:06 +0300

    Update trading_env.py

--- gym_anytrading/envs/trading_env.py
+++ gym_anytrading/envs/trading_env.py
@@ -9,11 +9,13 @@ import matplotlib.pyplot as plt
 class Actions(Enum):
     Sell = 0
     Buy = 1
+    Hold = 2
 
 
 class Positions(Enum):
     Short = 0
     Long = 1
+    Hold = 2
 
     def opposite(self):
         return Positions.Short if self == Positions.Long else Positions.Long
